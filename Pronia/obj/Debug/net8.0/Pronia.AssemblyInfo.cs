2523414d9e2716280fd3f32670e8b948a70a7411
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-11-21 01:11:34 +0400

    Testimonial Joke Update

--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5f8c8619bb40df7e22b7ab94b7cbd0e272dd76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b72bdb78c16a3ecaea669d5ca237708f8424ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
