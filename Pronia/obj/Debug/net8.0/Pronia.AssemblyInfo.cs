cac568585d68ab083ec1b4d2a2cf0d4860f4fe78
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-11-24 19:02:10 +0400

    WorkingHomeAndShopButtons

--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aefd2d430cffd2e233fdf9a07e26a8103debd8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006fd7ea2ce8fed399c70545364344af5cd74e7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
