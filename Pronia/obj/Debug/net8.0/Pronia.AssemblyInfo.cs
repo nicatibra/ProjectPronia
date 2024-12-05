a3cea7ed0b05472746453d7fa8b08745fffd1c1f
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-12-06 00:49:31 +0400

    Logo Fixed

--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b2fcb305317f8fac95bb6c22e7d90cb98a67a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99cfd2c08d89b23c65401e8aada84b5209b5967")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
