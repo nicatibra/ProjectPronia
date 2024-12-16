a87024e219bb0ae4c2aea57d1b89a3d9f8a71b97
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-12-16 08:47:41 +0400

    Ajax Fixed

--- Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
+++ Pronia/obj/Debug/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340b89b6fbfe325b6ca0a1a79bb1c064403048c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d7b27779260359e41878eb2cdaf0ed6ad15083")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
